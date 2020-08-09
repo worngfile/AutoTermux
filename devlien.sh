a57cfad52548c205b9ca6adad4a3bbb7a2658013
SDLOY <67725864+worngfile@users.noreply.github.com>
2020-08-09 17:11:44 +0530

    Add files via upload

---
+++ devlien.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+#Auto setup termux friendly tool
+figlet -f smslant Devlien Termux Tools... |lolcat
+#apt update && apt upgrade
+clear
+fortune | cowsay -f eyes | toilet --metal -f term
+echo "
+
+"
